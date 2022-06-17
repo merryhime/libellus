7f3199b4b53ac405acce25f007045c11ce9fb7d7
Merry <git@mary.rs>
2022-06-17 14:24:03 +0100

    Use coroutines instead

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <memory>
 
 #include <boost/asio/dispatch.hpp>
+#include <boost/asio/spawn.hpp>
 #include <boost/asio/strand.hpp>
 #include <boost/beast/core.hpp>
 #include <boost/beast/http.hpp>
@@ -17,88 +18,59 @@ namespace http = beast::http;      // from <boost/beast/http.hpp>
 namespace net = boost::asio;       // from <boost/asio.hpp>
 using tcp = boost::asio::ip::tcp;  // from <boost/asio/ip/tcp.hpp>
 
-struct Session : public std::enable_shared_from_this<Session> {
-public:
-    Session(net::io_context& ioc_, tcp::socket&& socket)
-        : ioc{ioc_}
-        , stream{std::move(socket)}
-    {}
+void do_session(net::io_context& ioc, beast::tcp_stream stream, net::yield_context yield)
+{
+    beast::error_code ec;
 
-    void run()
-    {
-        do_read();
-    }
-
-private:
-    void do_read()
-    {
-        http::async_read(stream, buf, req, [self = shared_from_this()](beast::error_code ec, std::size_t bytes_transferred) {
-            (void)bytes_transferred;
-
-            if (ec == http::error::end_of_stream)
-                return self->do_close();
-
-            ASSERT_MSG(!ec, "session::do_read {}", ec.message());
-
-            self->handle_request();
-        });
-    }
-
-    void do_close()
-    {
-        beast::error_code ec;
-        stream.socket().shutdown(tcp::socket::shutdown_send, ec);
-        ASSERT_MSG(!ec, "session::do_close {}", ec.message());
-    }
-
-    void handle_request()
-    {
-        fmt::print("{} {} [{}]\n", req.method(), req.target(), req.body());
-    }
-
-    net::io_context& ioc;
-    beast::tcp_stream stream;
     beast::flat_buffer buf;
     http::request<http::string_body> req;
-};
 
-struct Listener : public std::enable_shared_from_this<Listener> {
-public:
-    Listener(net::io_context& ioc_, net::ip::address addr_, u16 port_)
-        : ioc{ioc_}
-        , acceptor{ioc_}
-    {
-        beast::error_code ec;
-        tcp::endpoint endpoint{addr_, port_};
+    for (;;) {
+        http::async_read(stream, buf, req, yield[ec]);
 
-        acceptor.open(endpoint.protocol(), ec);
-        ASSERT_MSG(!ec, "acceptor.open {}", ec.message());
+        if (ec == http::error::end_of_stream)
+            break;
+        ASSERT_MSG(!ec, "session::do_read {}", ec.message());
 
-        acceptor.set_option(net::socket_base::reuse_address(true), ec);
-        ASSERT_MSG(!ec, "acceptor.set_option {}", ec.message());
+        fmt::print("{} {} [{}]\n", req.method(), req.target(), req.body());
 
-        acceptor.bind(endpoint, ec);
-        ASSERT_MSG(!ec, "acceptor.bind {}", ec.message());
-
-        acceptor.listen(net::socket_base::max_listen_connections, ec);
-        ASSERT_MSG(!ec, "acceptor.listen {}", ec.message());
+        break;
     }
 
-    void run()
-    {
-        acceptor.async_accept(ioc, [self = shared_from_this()](beast::error_code ec, tcp::socket socket) {
-            ASSERT_MSG(!ec, "accept failure {}", ec.message());
+    stream.socket().shutdown(tcp::socket::shutdown_send, ec);
+    ASSERT_MSG(!ec, "session::do_close {}", ec.message());
+}
 
-            std::make_shared<Session>(self->ioc, std::move(socket))->run();
+void do_listen(net::io_context& ioc, net::ip::address addr, u16 port, net::yield_context yield)
+{
+    beast::error_code ec;
 
-            self->run();
+    tcp::endpoint endpoint{addr, port};
+    tcp::acceptor acceptor{ioc};
+
+    acceptor.open(endpoint.protocol(), ec);
+    ASSERT_MSG(!ec, "acceptor.open {}", ec.message());
+
+    acceptor.set_option(net::socket_base::reuse_address(true), ec);
+    ASSERT_MSG(!ec, "acceptor.set_option {}", ec.message());
+
+    acceptor.bind(endpoint, ec);
+    ASSERT_MSG(!ec, "acceptor.bind {}", ec.message());
+
+    acceptor.listen(net::socket_base::max_listen_connections, ec);
+    ASSERT_MSG(!ec, "acceptor.listen {}", ec.message());
+
+    for (;;) {
+        tcp::socket socket{ioc};
+
+        acceptor.async_accept(socket, yield[ec]);
+        ASSERT_MSG(!ec, "accept failure {}", ec.message());
+
+        boost::asio::spawn(ioc, [&](net::yield_context yield) {
+            do_session(ioc, beast::tcp_stream{std::move(socket)}, yield);
         });
     }
-
-private:
-    net::io_context& ioc;
-    tcp::acceptor acceptor;
-};
+}
 
 int main()
 {
@@ -107,7 +79,9 @@ int main()
 
     net::io_context ioc{};
 
-    std::make_shared<Listener>(ioc, addr, port)->run();
+    boost::asio::spawn(ioc, [&](net::yield_context yield) {
+        do_listen(ioc, addr, port, yield);
+    });
 
     ioc.run();
 
